25016968d228004050647d9201b60949050257c2
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2023-01-09 10:31:47 +0600

    2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from 'components/Navigation/Navigation';
 import Footer from 'components/Footer/Footer';
-import styles from '../Layout/Layout.module.css';
+// import styles from '../Layout/Layout.module.css';
 
 const Layout = () => {
   return (
@@ -9,27 +9,12 @@ const Layout = () => {
       <header>
         <Navigation />
       </header>
-      <main style={{ minHeight: '100vh' }}>
-        <>
-          <div className={styles.leaves}>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-          </div>
-          <Outlet />
-        </>
+      <main
+        style={{
+          minHeight: '100vh',
+        }}
+      >
+        <Outlet />
       </main>
       <footer>
         <Footer />
